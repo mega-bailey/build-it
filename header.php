87506005da1319608c18ae97423693a0c9f6665e
Megan Bailey <megan-bailey@outlook.com>
2020-07-18 15:03:49 -0700

    add header

---
+++ header.php
@@ -0,0 +1,23 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Tera&family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Build It</title>
+</head>
+
+<body>
+    <nav class="navbar grey darken-4">
+        <div class="nav-wrapper">
+            <a href="index.php" class="left brand-logo"><i class=" material-icons">fitness_center</i></a>
+            <ul id="nav-mobile" class="right hide-on-small-only">
+                <li><a href="add.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">Add an Exercise</span></a></li>
+                <li><a href="exercises.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">All Exercises</span></a></li>
+                <li><a href="my-workout.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">My Workout</span></a></li>
+            </ul>
+        </div>
+    </nav>
+    <main>
