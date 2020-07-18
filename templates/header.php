9960ed50a5454c5f044ec269b94cf92b001e6b7e
Megan Bailey <megan-bailey@outlook.com>
2020-07-18 15:13:20 -0700

    Add header name func

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if ($_SERVER['QUERY_STRING'] == 'empty') {
+    unset($_SESSION['name']);
+}
+
+$name = $_SESSION['name'] ?? 'Guest';
+?>
+
 
 <head>
     <meta charset="UTF-8">
@@ -14,6 +24,7 @@
         <div class="nav-wrapper">
             <a href="index.php" class="left brand-logo"><i class=" material-icons">fitness_center</i></a>
             <ul id="nav-mobile" class="right hide-on-small-only">
+                <li class="white-text bold  hide-on-med-and-down">Hello <?php echo htmlspecialchars($name); ?> </li>
                 <li><a href="add.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">Add an Exercise</span></a></li>
                 <li><a href="exercises.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">All Exercises</span></a></li>
                 <li><a href="my-workout.php" class="btn waves-effect waves-light purple lighten-2"><span class="grey-text text-darken-4">My Workout</span></a></li>
