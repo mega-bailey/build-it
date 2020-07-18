eb58269738c756732a1288e36d3b0f11a1696a56
Megan Bailey <megan-bailey@outlook.com>
2020-07-18 14:56:16 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<div class="section col s12 ">
+    <h1 class="center thin lowercase">Welcome to</h1>
+    <h2 class="center bold upcase">Build It</h2>
+</div>
+
+
+<div class="section">
+    <div class="row">
+
+        <!-- Left Image -->
+        <div class="section col l7 hide-on-med-and-down">
+            <div class="main-img-container">
+                <img class="section main-img" src="img/undraw_working_out_6psf.svg" alt="illustration of two people working out"></div>
+        </div>
+
+
+        <!-- Right Form -->
+        <div class="section col m12 l5 flex-center">
+            <div class="container">
+                <form action="" method="POST" class="z-depth-0">
+                    <h3 class="section">Let's get started!</h3>
+                    <label for="name">Your Name:</label>
+                    <input type="text" name="name">
+                    <div class="red-text"></div>
+
+                    <div class="section">
+                        <input type="submit" name="submit" value='submit' class='btn waves-effect waves-light purple lighten-2'>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+</html>
